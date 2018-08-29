da8022bd082b64e8ba71cba93dffb5ff591a1b26
Madhusudhan <sudhansagar@gmail.com>
2018-08-29 16:41:27 +0530

    Hello WebService SOAP Client

---
+++ hellowebserviceclient/src/main/java/com/madhu/soapservice/HellowebserviceclientApplication.java
@@ -0,0 +1,12 @@
+package com.madhu.soapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowebserviceclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowebserviceclientApplication.class, args);
+	}
+}
---
+++ hellowebserviceclient/src/main/java/com/madhu/soapservice/WsClient.java
@@ -0,0 +1,19 @@
+package com.madhu.soapservice;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.madhu.ws.soap.Hellows;
+import com.madhu.ws.soap.HellowsService;
+
+public class WsClient {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		HellowsService ser = new HellowsService(new URL("http://localhost:8080/hellows/hello?wsdl"));
+		Hellows hellowsPort = ser.getHellowsPort();
+		String hello = hellowsPort.hello();
+		System.out.println("Response from Hello SOAP Service : " + hello);
+	}
+
+}
